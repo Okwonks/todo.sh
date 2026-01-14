07fc9c13de8f8e6898c3481e8b74ba796b16a41f
Albert Oketch <albert@paygoenergy.co>
2026-01-14 22:18:49 +0000

    cmd/tui: update display and key msg handling

--- cmd/tui/main.go
+++ cmd/tui/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
-	"time"
+	"strconv"
+	"strings"
 
 	"github.com/Okwonks/go-todo/internal/client"
 	dbmodel "github.com/Okwonks/go-todo/internal/model"
+	"github.com/charmbracelet/bubbles/cursor"
 	"github.com/charmbracelet/bubbles/table"
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
@@ -31,7 +34,11 @@ type model struct {
 func initialModel() model {
 	c := client.NewClient("http://localhost:8080")
 
-	columns := []table.Column{}
+	columns := []table.Column{
+		{Title: "ID",          Width: 5},
+		{Title: "CreatedAt",   Width: 20},
+		{Title: "Description", Width: 30},
+	}
 
 	t := table.New(
 		table.WithColumns(columns),
@@ -39,8 +46,8 @@ func initialModel() model {
 	)
 
 	return model{
-		state: stateList,
-		table: t,
+		state:  stateList,
+		table:  t,
 		client: c,
 	}
 }
@@ -66,7 +73,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 	  switch msg.String() {
-		case "q":
+		case "ctrl+c", "q":
 		  return m, tea.Quit
 		case "r":
 		  return m, fetchTodos(m.client)
@@ -81,7 +88,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		rows := make([]table.Row, 0, len(msg))
 
 	  for _, t := range msg {
-			rows = append(rows, table.Row{fmt.Sprint(t.ID, t.Description, t.CreatedAt)})
+			rows = append(rows, table.Row{fmt.Sprint(t.ID), t.Description, t.CreatedAt.String()})
 		}
 
 	  m.table.SetRows(rows)
@@ -97,17 +104,27 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.taskInput = newTask
 
 		if m.taskInput.submitted {
-			t := &dbmodel.Todo{
-				Description: m.taskInput.description,
-				DueDate:     m.taskInput.dueDate,
-				Priority:    m.taskInput.priority,
+			p, err := strconv.Atoi(m.taskInput.inputs[1].Value())
+			if err != nil {
+				m.err = err
+				return m, fetchTodos(m.client)
 			}
 
-			_, err := m.client.CreateTodo(t)
+			t := &dbmodel.Todo{
+				Description: m.taskInput.inputs[0].Value(),
+				Priority:    p,
+			}
+
+			_, err = m.client.CreateTodo(t)
 			if err != nil {
 				m.err = err
 			}
 			
+			m.state = stateList
+			return m, fetchTodos(m.client)
+		}
+
+		if !m.taskInput.submitted && m.taskInput.showList {
 			m.state = stateList
 			return m, fetchTodos(m.client)
 		}
@@ -119,7 +136,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	if m.state == stateList {
+	if m.state == stateCreate {
 		return m.taskInput.View()
 	}
 
@@ -139,59 +156,155 @@ func (m model) View() string {
 }
 
 type taskInputModel struct {
-	description string
-	dueDate     *time.Time 
-	priority    int
 	submitted   bool
-	cursor      int
+	inputs      []textinput.Model
+	focusIndex  int
+	cursorMode  cursor.Mode
+	showList    bool
 }
 
+var (
+	focusedStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	blurredStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("240"))
+	cursorStyle         = focusedStyle
+	noStyle             = lipgloss.NewStyle()
+	// helpStyle           = blurredStyle
+	// cursorModeHelpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("244"))
+
+	focusedButton = focusedStyle.Render("[ Submit ]")
+	blurredButton = fmt.Sprintf("[ %s ]", blurredStyle.Render("Submit"))
+)
+
 func newTaskInputModel() taskInputModel {
-	return taskInputModel{}
+	m := taskInputModel{
+		inputs: make([]textinput.Model, 2),
+	}
+
+	var ti textinput.Model
+	for i := range m.inputs {
+		ti = textinput.New()
+		ti.Cursor.Style = cursorStyle
+		ti.CharLimit = 200
+		ti.Width = 200
+
+		switch i {
+		case 0:
+		  ti.Placeholder = "Task"
+			ti.Focus()
+		  ti.PromptStyle = focusedStyle
+			ti.TextStyle = focusedStyle
+		case 1:
+		  ti.Placeholder = "Priority"
+			ti.CharLimit = 1
+			ti.Validate = func(s string) error {
+				if s == "" {
+					return nil
+				}
+				for _, r := range s {
+					if r < '1' || r > '5' {
+						return fmt.Errorf("only digits between 1 and 5 allowed")
+					}
+				}
+				return nil
+			}
+		}
+		m.inputs[i] = ti
+	}
+
+	return m
 }
 
 func (m taskInputModel) Init() tea.Cmd {
-	return nil
+	return textinput.Blink
 }
 
 func (m taskInputModel) Update(msg tea.Msg) (taskInputModel, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 	  switch msg.String() {
-		case "tab":
-		  m.cursor = (m.cursor + 1) % 2
-		case "enter":
-		  m.submitted = true
-			return m, nil
 		case "esc":
 		  m.submitted = false
+			m.focusIndex = 0
+			m.showList = true
 			return m, nil
-		case "backspace":
-		  // TODO: change this to use []inputs, making setting the values easier
-		  if m.cursor == 0 && len(m.description) > 0 {
-				m.description = m.description[:len(m.description)-1]
+		case "tab", "shift+tab", "enter", "up", "down":
+			s := msg.String()
+
+			if s == "enter" && m.focusIndex == len(m.inputs) {
+				m.submitted = true
+				return m, nil
 			}
-		  if m.cursor == 1 && m.priority > 0 {
-				m.priority -= 1
+
+		  if s == "up" || s == "shift+tab" {
+				m.focusIndex--;
+			} else {
+				m.focusIndex++;
 			}
-		default:
-		  if msg.Type == tea.KeyRunes {
-				if m.cursor == 0 {
-					m.description += msg.String()
-				} else  {
-					m.priority = min(m.priority + 1, 5)
+
+		  if m.focusIndex > len(m.inputs) {
+				m.focusIndex = 0
+			} else if m.focusIndex < 0 {
+				m.focusIndex = len(m.inputs)
+		  }
+
+		  cmds := make([]tea.Cmd, len(m.inputs))
+		  for i := 0; i < len(m.inputs); i++ {
+				if i == m.focusIndex {
+					cmds[i] = m.inputs[i].Focus()
+					m.inputs[i].PromptStyle = focusedStyle
+					m.inputs[i].TextStyle = focusedStyle
+					continue
 				}
+				// Remove focused state
+				m.inputs[i].Blur()
+				m.inputs[i].PromptStyle = noStyle
+				m.inputs[i].TextStyle = noStyle
 			}
+
+		  return m, tea.Batch(cmds...)
 		}
 	}
-	return m, nil
+
+	cmd := m.updateInputs(msg)
+
+	return m, cmd
+}
+
+func (m *taskInputModel) updateInputs(msg tea.Msg) tea.Cmd {
+	cmds := make([]tea.Cmd, len(m.inputs))
+
+	for i := range m.inputs {
+		m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
+	}
+
+	return tea.Batch(cmds...)
 }
 
 func (m taskInputModel) View() string {
-	return fmt.Sprintf(
-		"Create Task\n\nDescription:  %s\nPriority:   %d\n\n[enter] submit • [esc] cancel",
-		m.description, m.priority,
-	)
+	var b strings.Builder
+
+	style := lipgloss.NewStyle().Bold(true).Margin(1)
+	title := style.Render("Task")
+	b.WriteString(title)
+	b.WriteRune('\n')
+
+	for i:= range m.inputs {
+		b.WriteString(m.inputs[i].View())
+		if i < len(m.inputs)-1 {
+			b.WriteRune('\n')
+		}
+	}
+
+	button := &blurredButton
+	if m.focusIndex == len(m.inputs) {
+		button = &focusedButton
+	}
+	fmt.Fprintf(&b, "\n\n%s\n\n", *button)
+
+
+	help := "\n[q] quit • [esc] go back"
+
+	return b.String() + help
 }
 
 func main() {
